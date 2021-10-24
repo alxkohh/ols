63193f904295c6962e949aca2581d9abecebdcf2
Alex <alxkohh@gmail.com>
2021-10-25 00:07:57 +0800

    Fix position

--- emp/group/position_group.sql
+++ emp/group/position_group.sql
@@ -1,31 +1,44 @@
--- position groups for emp read label
+-- position groups for emp r/w label
 BEGIN
-SA_COMPONENTS.CREATE_GROUP(
-    policy_name => 'emp_r_label',
-    group_num   => 30,
-    short_name  => 'DIR',
-    long_name   => 'DIRECTOR'
-);
-END;
-/
-
-BEGIN
-    SA_COMPONENTS.CREATE_GROUP(
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_r_label',
+        group_num   => 30,
+        short_name  => 'DIR',
+        long_name   => 'DIRECTOR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
         policy_name => 'emp_r_label',
         group_num   => 20,
         short_name  => 'MGR',
         long_name   => 'MANAGER',
         parent_name => 'DIR'
     );
-END;
-/
-
-BEGIN
-SA_COMPONENTS.CREATE_GROUP(
-    policy_name => 'emp_r_label',
-    group_num   => 10,
-    short_name  => 'EMP',
-    long_name   => 'EMPLOYEE',
-    parent_name => 'MGR');
+    SA_COMPONENTS.CREATE_GROUP(
+        policy_name => 'emp_r_label',
+        group_num   => 10,
+        short_name  => 'EMP',
+        long_name   => 'EMPLOYEE',
+        parent_name => 'MGR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_w_label',
+        group_num   => 30,
+        short_name  => 'DIR',
+        long_name   => 'DIRECTOR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_w_label',
+        group_num   => 20,
+        short_name  => 'MGR',
+        long_name   => 'MANAGER',
+        parent_name => 'DIR'
+    );
+    SA_COMPONENTS.CREATE_GROUP(
+        policy_name => 'emp_w_label',
+        group_num   => 10,
+        short_name  => 'EMP',
+        long_name   => 'EMPLOYEE',
+        parent_name => 'MGR'
+    );
 END;
 /
