5c978f7b3f6e3873f63ab7963ecbfc16d2410bc3
Alex <alxkohh@gmail.com>
2021-10-25 00:20:51 +0800

    Fix region

--- emp/group/region_group.sql
+++ emp/group/region_group.sql
@@ -1,98 +1,143 @@
--- region groups for emp read label
+-- region groups for emp r/w label
 BEGIN
- SA_COMPONENTS.CREATE_GROUP (
-  policy_name     => 'emp_r_label',
-  group_num       => 40,
-  short_name      => 'GLOBAL',
-  long_name       => 'GLOBAL');
-END;
-/
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_r_label',
+        group_num   => 40,
+        short_name  => 'GLOBAL',
+        long_name   => 'GLOBAL'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_r_label',
+        group_num   => 30,
+        short_name  => 'APAC',
+        long_name   => 'APAC',
+        parent_name => 'GLOBAL'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_r_label',
+        group_num   => 31,
+        short_name  => 'SG',
+        long_name   => 'SINGAPORE',
+        parent_name => 'APAC'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_r_label',
+        group_num   => 32,
+        short_name  => 'HK',
+        long_name   => 'HONG_KONG',
+        parent_name => 'APAC'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_r_label',
+        group_num   => 20,
+        short_name  => 'EMEA',
+        long_name   => 'EMEA',
+        parent_name => 'GLOBAL'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_r_label',
+        group_num   => 21,
+        short_name  => 'LON',
+        long_name   => 'LONDON',
+        parent_name => 'EMEA'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_r_label',
+        group_num   => 22,
+        short_name  => 'PAR',
+        long_name   => 'PARIS',
+        parent_name => 'EMEA'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_r_label',
+        group_num   => 10,
+        short_name  => 'AMER',
+        long_name   => 'AMER',
+        parent_name => 'GLOBAL'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_r_label',
+        group_num   => 11,
+        short_name  => 'NY',
+        long_name   => 'NEW_YORK',
+        parent_name => 'AMER'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_r_label',
+        group_num   => 12,
+        short_name  => 'SF',
+        long_name   => 'SAN_FRANCISCO',
+        parent_name => 'AMER'
+    );
 
-BEGIN
- SA_COMPONENTS.CREATE_GROUP (
-  policy_name     => 'emp_r_label',
-  group_num       => 30,
-  short_name      => 'APAC',
-  long_name       => 'APAC',
-  parent_name     => 'GLOBAL');
-END;
-/
-
-BEGIN
- SA_COMPONENTS.CREATE_GROUP (
-  policy_name     => 'emp_r_label',
-  group_num       => 20,
-  short_name      => 'EMEA',
-  long_name       => 'EMEA',
-  parent_name     => 'GLOBAL');
-END;
-/
-
-BEGIN
- SA_COMPONENTS.CREATE_GROUP (
-  policy_name     => 'emp_r_label',
-  group_num       => 10,
-  short_name      => 'AMER',
-  long_name       => 'AMER',
-  parent_name     => 'GLOBAL');
-END;
-/
-BEGIN
- SA_COMPONENTS.CREATE_GROUP (
-  policy_name     => 'emp_r_label',
-  group_num       => 31,
-  short_name      => 'SG',
-  long_name       => 'SINGAPORE',
-  parent_name     => 'APAC');
-END;
-/
-
-BEGIN
- SA_COMPONENTS.CREATE_GROUP(
-  policy_name     => 'emp_r_label',
-  group_num       => 32,
-  short_name      => 'HK',
-  long_name       => 'HONG_KONG',
-  parent_name     => 'APAC');
-END;
-/
-
-BEGIN
- SA_COMPONENTS.CREATE_GROUP (
-  policy_name     => 'emp_r_label',
-  group_num       => 21,
-  short_name      => 'LON',
-  long_name       => 'LONDON',
-  parent_name     => 'EMEA');
-END;
-/
-
-BEGIN
- SA_COMPONENTS.CREATE_GROUP (
-  policy_name     => 'emp_r_label',
-  group_num       => 22,
-  short_name      => 'PAR',
-  long_name       => 'PARIS',
-  parent_name     => 'EMEA');
-END;
-/
-
-BEGIN
- SA_COMPONENTS.CREATE_GROUP (
-  policy_name     => 'emp_r_label',
-  group_num       => 11,
-  short_name      => 'NY',
-  long_name       => 'NEW_YORK',
-  parent_name     => 'AMER');
-END;
-/
-
-BEGIN
- SA_COMPONENTS.CREATE_GROUP (
-  policy_name     => 'emp_r_label',
-  group_num       => 12,
-  short_name      => 'SF',
-  long_name       => 'SAN_FRANCISCO',
-  parent_name     => 'AMER');
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_w_label',
+        group_num   => 40,
+        short_name  => 'GLOBAL',
+        long_name   => 'GLOBAL'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_w_label',
+        group_num   => 30,
+        short_name  => 'APAC',
+        long_name   => 'APAC',
+        parent_name => 'GLOBAL'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_w_label',
+        group_num   => 31,
+        short_name  => 'SG',
+        long_name   => 'SINGAPORE',
+        parent_name => 'APAC'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_w_label',
+        group_num   => 32,
+        short_name  => 'HK',
+        long_name   => 'HONG_KONG',
+        parent_name => 'APAC'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_w_label',
+        group_num   => 20,
+        short_name  => 'EMEA',
+        long_name   => 'EMEA',
+        parent_name => 'GLOBAL'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_w_label',
+        group_num   => 21,
+        short_name  => 'LON',
+        long_name   => 'LONDON',
+        parent_name => 'EMEA'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_w_label',
+        group_num   => 22,
+        short_name  => 'PAR',
+        long_name   => 'PARIS',
+        parent_name => 'EMEA'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_w_label',
+        group_num   => 10,
+        short_name  => 'AMER',
+        long_name   => 'AMER',
+        parent_name => 'GLOBAL'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_w_label',
+        group_num   => 11,
+        short_name  => 'NY',
+        long_name   => 'NEW_YORK',
+        parent_name => 'AMER'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'emp_w_label',
+        group_num   => 12,
+        short_name  => 'SF',
+        long_name   => 'SAN_FRANCISCO',
+        parent_name => 'AMER'
+    );
 END;
 /
