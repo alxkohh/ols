591158c277dc0260b099ddf375710f377812c0c3
unknown <yeodonghan@gmail.com>
2021-10-25 23:54:08 +0800

    added labels for salary, project and project_budget

--- emp/group/position_group.sql
+++ emp/group/position_group.sql
@@ -2,40 +2,175 @@
 BEGIN
     SA_COMPONENTS.CREATE_GROUP (
         policy_name => 'emp_r_label',
-        group_num   => 30,
+        group_num   => 1000,
         short_name  => 'DIR',
         long_name   => 'DIRECTOR'
     );
     SA_COMPONENTS.CREATE_GROUP (
         policy_name => 'emp_r_label',
-        group_num   => 20,
+        group_num   => 1100,
         short_name  => 'MGR',
         long_name   => 'MANAGER',
         parent_name => 'DIR'
     );
     SA_COMPONENTS.CREATE_GROUP (
         policy_name => 'emp_r_label',
-        group_num   => 10,
+        group_num   => 1110,
         short_name  => 'EMP',
         long_name   => 'EMPLOYEE',
         parent_name => 'MGR'
     );
     SA_COMPONENTS.CREATE_GROUP (
         policy_name => 'emp_w_label',
-        group_num   => 30,
+        group_num   => 1000,
         short_name  => 'DIR',
         long_name   => 'DIRECTOR'
     );
     SA_COMPONENTS.CREATE_GROUP (
         policy_name => 'emp_w_label',
-        group_num   => 20,
+        group_num   => 1100,
         short_name  => 'MGR',
         long_name   => 'MANAGER',
         parent_name => 'DIR'
     );
     SA_COMPONENTS.CREATE_GROUP (
         policy_name => 'emp_w_label',
-        group_num   => 10,
+        group_num   => 1110,
+        short_name  => 'EMP',
+        long_name   => 'EMPLOYEE',
+        parent_name => 'MGR'
+    );
+END;
+/
+
+-- position groups for sal r/w label
+BEGIN
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'sal_r_label',
+        group_num   => 1000,
+        short_name  => 'DIR',
+        long_name   => 'DIRECTOR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'sal_r_label',
+        group_num   => 1100,
+        short_name  => 'MGR',
+        long_name   => 'MANAGER',
+        parent_name => 'DIR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'sal_r_label',
+        group_num   => 1110,
+        short_name  => 'EMP',
+        long_name   => 'EMPLOYEE',
+        parent_name => 'MGR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'sal_w_label',
+        group_num   => 1000,
+        short_name  => 'DIR',
+        long_name   => 'DIRECTOR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'sal_w_label',
+        group_num   => 1100,
+        short_name  => 'MGR',
+        long_name   => 'MANAGER',
+        parent_name => 'DIR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'sal_w_label',
+        group_num   => 1110,
+        short_name  => 'EMP',
+        long_name   => 'EMPLOYEE',
+        parent_name => 'MGR'
+    );
+END;
+/
+
+-- position groups for proj r/w label
+BEGIN
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'proj_r_label',
+        group_num   => 1000,
+        short_name  => 'DIR',
+        long_name   => 'DIRECTOR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'proj_r_label',
+        group_num   => 1100,
+        short_name  => 'MGR',
+        long_name   => 'MANAGER',
+        parent_name => 'DIR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'proj_r_label',
+        group_num   => 1110,
+        short_name  => 'EMP',
+        long_name   => 'EMPLOYEE',
+        parent_name => 'MGR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'proj_w_label',
+        group_num   => 1000,
+        short_name  => 'DIR',
+        long_name   => 'DIRECTOR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'proj_w_label',
+        group_num   => 1100,
+        short_name  => 'MGR',
+        long_name   => 'MANAGER',
+        parent_name => 'DIR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'proj_w_label',
+        group_num   => 1110,
+        short_name  => 'EMP',
+        long_name   => 'EMPLOYEE',
+        parent_name => 'MGR'
+    );
+END;
+/
+
+-- position groups for budget r/w label
+BEGIN
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'budget_r_label',
+        group_num   => 1000,
+        short_name  => 'DIR',
+        long_name   => 'DIRECTOR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'budget_r_label',
+        group_num   => 1100,
+        short_name  => 'MGR',
+        long_name   => 'MANAGER',
+        parent_name => 'DIR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'budget_r_label',
+        group_num   => 1110,
+        short_name  => 'EMP',
+        long_name   => 'EMPLOYEE',
+        parent_name => 'MGR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'budget_w_label',
+        group_num   => 1000,
+        short_name  => 'DIR',
+        long_name   => 'DIRECTOR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'budget_w_label',
+        group_num   => 1100,
+        short_name  => 'MGR',
+        long_name   => 'MANAGER',
+        parent_name => 'DIR'
+    );
+    SA_COMPONENTS.CREATE_GROUP (
+        policy_name => 'budget_w_label',
+        group_num   => 1110,
         short_name  => 'EMP',
         long_name   => 'EMPLOYEE',
         parent_name => 'MGR'
--- emp/label.sql
+++ emp/label.sql
@@ -12,3 +12,48 @@ BEGIN
     );
 END;
 /
+
+-- sal r/w label
+BEGIN
+    SA_SYSDBA.CREATE_POLICY(
+        policy_name     => 'sal_r_label',
+        column_name     => 'r_label',
+        default_options => 'read_control'
+    );
+    SA_SYSDBA.CREATE_POLICY(
+        policy_name     => 'sal_w_label',
+        column_name     => 'w_label',
+        default_options => 'write_control'
+    );
+END;
+/
+
+-- proj r/w label
+BEGIN
+    SA_SYSDBA.CREATE_POLICY(
+        policy_name     => 'proj_r_label',
+        column_name     => 'r_label',
+        default_options => 'read_control'
+    );
+    SA_SYSDBA.CREATE_POLICY(
+        policy_name     => 'proj_w_label',
+        column_name     => 'w_label',
+        default_options => 'write_control'
+    );
+END;
+/
+
+-- budget r/w label
+BEGIN
+    SA_SYSDBA.CREATE_POLICY(
+        policy_name     => 'budget_r_label',
+        column_name     => 'r_label',
+        default_options => 'read_control'
+    );
+    SA_SYSDBA.CREATE_POLICY(
+        policy_name     => 'budget_w_label',
+        column_name     => 'w_label',
+        default_options => 'write_control'
+    );
+END;
+/
