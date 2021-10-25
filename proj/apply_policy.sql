10b75f6cc22000d65bc6dd9967c46748e50c4ad4
unknown <yeodonghan@gmail.com>
2021-10-26 01:25:07 +0800

    fixed schema_name in apply_policy to system

--- proj/apply_policy.sql
+++ proj/apply_policy.sql
@@ -1,14 +1,14 @@
 BEGIN
   SA_USER_ADMIN.APPLY_TABLE_POLICY (
     policy_name    => 'proj_r_label',
-    schema_name    => 'sys', 
+    schema_name    => 'system', 
     table_name     => 'projects',
     table_options  => 'READ_CONTROL',
     label_function => 'hr.gen_emp_label(:new.department_id,:new.salary)',
   );
   SA_USER_ADMIN.APPLY_TABLE_POLICY (
     policy_name    => 'proj_w_label',
-    schema_name    => 'sys', 
+    schema_name    => 'system', 
     table_name     => 'projects',
     table_options  => 'WRITE_CONTROL',
     label_function => 'hr.gen_emp_label(:new.department_id,:new.salary)',
