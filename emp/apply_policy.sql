f90faec60f61c781359b645615c4a5ba947b7963
Alex <alxkohh@gmail.com>
2021-10-26 01:53:01 +0800

    Add dummy functions

--- emp/apply_policy.sql
+++ emp/apply_policy.sql
@@ -1,17 +1,37 @@
+CREATE FUNCTION gen_emp_r_label()
+RETURN VARCHAR
+IS
+label VARCHAR
+BEGIN
+label := 'U';
+RETURN label
+END;
+/
+
+CREATE FUNCTION gen_emp_w_label()
+RETURN VARCHAR
+IS
+label VARCHAR
+BEGIN
+label := 'U';
+RETURN label
+END;
+/
+
 BEGIN
   SA_USER_ADMIN.APPLY_TABLE_POLICY (
     policy_name    => 'emp_r_label',
     schema_name    => 'system', 
     table_name     => 'employees',
     table_options  => 'READ_CONTROL',
-    label_function => 'hr.gen_emp_label(:new.department_id,:new.salary)',
+    label_function => 'system.gen_emp_r_label()',
   );
   SA_USER_ADMIN.APPLY_TABLE_POLICY (
     policy_name    => 'emp_w_label',
     schema_name    => 'system', 
     table_name     => 'employees',
     table_options  => 'WRITE_CONTROL',
-    label_function => 'hr.gen_emp_label(:new.department_id,:new.salary)',
+    label_function => 'system.gen_emp_w_label()',
   );
 END;
 /
