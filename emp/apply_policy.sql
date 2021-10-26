c1c089cff1dce0cb33a8446f26a0b5e625151189
Alex <alxkohh@gmail.com>
2021-10-27 00:53:23 +0800

    Rename apply policy

--- emp/apply_policy.sql
+++ emp/apply_policy.sql
@@ -19,14 +19,14 @@ END;
 /
 
 BEGIN
-  SA_USER_ADMIN.APPLY_TABLE_POLICY (
+  SA_POLICY_ADMIN.APPLY_TABLE_POLICY (
     policy_name    => 'emp_r_label_policy',
     schema_name    => 'sys', 
     table_name     => 'employees',
     table_options  => 'READ_CONTROL',
     label_function => 'sys.gen_emp_r_label()',
   );
-  SA_USER_ADMIN.APPLY_TABLE_POLICY (
+  SA_POLICY_ADMIN.APPLY_TABLE_POLICY (
     policy_name    => 'emp_w_label_policy',
     schema_name    => 'sys', 
     table_name     => 'employees',
