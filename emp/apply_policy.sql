5e7bb0cc5f2a8195bf7a63b3b67cba22415d97fe
Joyce <jycyeo@yahoo.com.sg>
2021-10-28 01:51:16 +0800

    add label function (untested)

--- emp/apply_policy.sql
+++ emp/apply_policy.sql
@@ -1,37 +1,32 @@
-CREATE OR REPLACE FUNCTION gen_emp_r_label
-RETURN VARCHAR
-IS
-label VARCHAR
+-- run as sys
+CREATE OR REPLACE FUNCTION emp_update_label(position VARCHAR2, dept VARCHAR2, region VARCHAR2)
+RETURN lbacsys.lbac_label AS
+emp_r_label VARCHAR(20);
+emp_w_label VARCHAR(20);
+sal_all_label VARCHAR(20);
 BEGIN
-label := 'U';
-RETURN label
-END;
-/
+  emp_r_label := 'U:' || dept || ':' || position || ',' || region;
+  emp_w_label := 'U:HR:' || position || ',' || region;
+  sal_all_label := 'C:HR' || position || ',' || region;
 
-CREATE OR REPLACE gen_emp_w_label
-RETURN VARCHAR
-IS
-label VARCHAR
-BEGIN
-label := 'U';
-RETURN label
+  UPDATE company.employees SET emp_r_label = CHAR_TO_LABEL('emp_r_label_policy', emp_r_label);
+  UPDATE company.salary SET sal_all_label = CHAR_TO_LABEL('sal_all_label_policy', sal_all_label);
+  RETURN to_lbac_data_label('emp_w_label_policy', emp_w_label);
 END;
 /
 
 BEGIN
   SA_POLICY_ADMIN.APPLY_TABLE_POLICY (
     policy_name    => 'emp_r_label_policy',
-    schema_name    => 'sys', 
+    schema_name    => 'company', 
     table_name     => 'employees',
-    table_options  => 'READ_CONTROL',
-    label_function => 'sys.gen_emp_r_label()',
+    table_options  => 'READ_CONTROL'
   );
   SA_POLICY_ADMIN.APPLY_TABLE_POLICY (
     policy_name    => 'emp_w_label_policy',
-    schema_name    => 'sys', 
+    schema_name    => 'company', 
     table_name     => 'employees',
-    table_options  => 'WRITE_CONTROL',
-    label_function => 'sys.gen_emp_w_label()',
+    table_options  => 'WRITE_CONTROL'
   );
 END;
 /
