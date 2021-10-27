033e36cda9ec863b96c3548ab92449afb26ae397
Joyce <jycyeo@yahoo.com.sg>
2021-10-28 02:15:04 +0800

    label function -- syntax of apply not correct

--- emp/apply_policy.sql
+++ emp/apply_policy.sql
@@ -1,5 +1,5 @@
--- run as sys
-CREATE OR REPLACE FUNCTION emp_update_label(position VARCHAR2, dept VARCHAR2, region VARCHAR2)
+-- run as company
+CREATE OR REPLACE FUNCTION emp_update_label(new_emp_name VARCHAR2, position VARCHAR2, dept VARCHAR2, region VARCHAR2)
 RETURN lbacsys.lbac_label AS
 emp_r_label VARCHAR(20);
 emp_w_label VARCHAR(20);
@@ -9,9 +9,16 @@ BEGIN
   emp_w_label := 'U:HR:' || position || ',' || region;
   sal_all_label := 'C:HR' || position || ',' || region;
 
-  UPDATE company.employees SET emp_r_label = CHAR_TO_LABEL('emp_r_label_policy', emp_r_label);
-  UPDATE company.salary SET sal_all_label = CHAR_TO_LABEL('sal_all_label_policy', sal_all_label);
+  UPDATE company.employees SET emp_r_label = CHAR_TO_LABEL('emp_r_label_policy', emp_r_label) WHERE emp_name = new_emp_name;
+  UPDATE company.salary SET sal_all_label = CHAR_TO_LABEL('sal_all_label_policy', sal_all_label) WHERE emp_name = new_emp_name;
   RETURN to_lbac_data_label('emp_w_label_policy', emp_w_label);
+END;
+/
+
+-- run as sys
+BEGIN
+  SA_POLICY_ADMIN.REMOVE_TABLE_POLICY('emp_r_label_policy', 'company', 'employees');
+  SA_POLICY_ADMIN.REMOVE_TABLE_POLICY('emp_w_label_policy', 'company', 'employees');
 END;
 /
 
@@ -26,7 +33,8 @@ BEGIN
     policy_name    => 'emp_w_label_policy',
     schema_name    => 'company', 
     table_name     => 'employees',
-    table_options  => 'WRITE_CONTROL'
+    table_options  => 'WRITE_CONTROL',
+    label_function => company.emp_update_label(:new.emp_name, :new.position, :new.department, :new.region)
   );
 END;
 /
