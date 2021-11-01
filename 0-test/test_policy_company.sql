c1452faad648a540f7328ac918e9e009355eb368
Joyce <jycyeo@yahoo.com.sg>
2021-11-02 00:17:25 +0800

    add queries for checking

--- 0-test/test_policy_company.sql
+++ 0-test/test_policy_company.sql
@@ -7,3 +7,5 @@ GRANT SELECT ON test_tab TO PUBLIC;
 
 INSERT INTO test_tab (id, test_label) VALUES (1, char_to_label('test_policy', 's::mgr'));
 
+select id, label_to_char(test_label) from company.test_tab;
+select SA_SESSION.LABEL('test_policy') from dual;
