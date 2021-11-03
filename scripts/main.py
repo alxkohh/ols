0756f486e3accbb1878c526bdf1feff4a5b09509
Joyce <jycyeo@yahoo.com.sg>
2021-11-03 22:08:16 +0800

    rename files to include .sql

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,10 @@
 import employees
 import projects
 
-output_file = '7-insert_tables.sql'
-to_gen = [employees, projects]
-
-with open('7-insert_employees', 'w+') as f:
+with open('7-insert_employees.sql', 'w+') as f:
     for s in employees.gen_statements():
         f.write(s)
 
-with open('8-insert_projects', 'w+') as f:
+with open('8-insert_projects.sql', 'w+') as f:
     for s in projects.gen_statements():
         f.write(s)
