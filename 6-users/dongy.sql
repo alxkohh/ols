a6b5f0bf9b1a30aa665c7a7ef9cd16f361f1e343
unknown <yeodonghan@gmail.com>
2021-11-03 20:44:12 +0800

    Fixed setuserlabels for joycey in test policy

--- 6-users/dongy.sql
+++ 6-users/dongy.sql
@@ -79,5 +79,51 @@ BEGIN
         def_groups    => 'GLOBAL,DIR',
         row_groups    => ''
     );
+
+    SA_USER_ADMIN.SET_USER_PRIVS (
+        policy_name   => 'emp_r_label_policy',
+        user_name     => 'dongy',
+        privileges    => 'READ'
+    );
+
+    SA_USER_ADMIN.SET_USER_PRIVS (
+        policy_name   => 'emp_w_label_policy',
+        user_name     => 'dongy',
+        privileges    => 'FULL'
+    );
+
+    SA_USER_ADMIN.SET_USER_PRIVS (
+        policy_name   => 'sal_rw_label_policy',
+        user_name     => 'dongy',
+        privileges    => 'FULL'
+    );
+
+    SA_USER_ADMIN.SET_USER_LABELS (
+        policy_name       => 'emp_r_label_policy',
+        user_name         => 'dongy',
+        max_read_label    => 'S:ENG,FIN,HR:DIR,GLOBAL',
+        max_write_label   => 'S:ENG,FIN,HR:DIR,GLOBAL',
+        def_label         => 'S:ENG,FIN,HR:DIR,GLOBAL',
+        row_label         => 'S:ENG,FIN,HR:DIR,GLOBAL'
+    );
+
+    SA_USER_ADMIN.SET_USER_LABELS (
+        policy_name       => 'emp_w_label_policy',
+        user_name         => 'dongy',
+        max_read_label    => 'S:ENG,FIN,HR:DIR,GLOBAL',
+        max_write_label   => 'S:ENG,FIN,HR:DIR,GLOBAL',
+        def_label         => 'S:ENG,FIN,HR:DIR,GLOBAL',
+        row_label         => 'U:ENG,FIN,HR:DIR,GLOBAL'
+    );
+
+    SA_USER_ADMIN.SET_USER_LABELS (
+        policy_name       => 'sal_rw_label_policy',
+        user_name         => 'dongy',
+        max_read_label    => 'S:ENG,FIN,HR:DIR,GLOBAL',
+        max_write_label   => 'S:ENG,FIN,HR:DIR,GLOBAL',
+        def_label         => 'S:ENG,FIN,HR:DIR,GLOBAL',
+        row_label         => 'U:ENG,FIN,HR:DIR,GLOBAL'
+    );
+
 END;
 /
