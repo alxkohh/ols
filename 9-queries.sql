5a6b3fe8ce29d11aaf212a489215390a4a43e921
Joyce <jycyeo@yahoo.com.sg>
2021-11-09 04:10:52 +0800

    remove old stuff

--- 9-queries.sql
+++ 9-queries.sql
@@ -6,12 +6,3 @@ SELECT emp_name, label_to_char(emp_label) FROM company.employees;
 SELECT emp_name, label_to_char(sal_label) FROM company.salary;
 SELECT proj_name, label_to_char(proj_label) FROM company.projects;
 SELECT proj_name, label_to_char(budget_label) FROM company.project_budgets;
-
--- LABEL FUNCTIONS
-
-DELETE FROM company.salary WHERE emp_name = 'thomask';
-DELETE FROM company.employees WHERE emp_name = 'thomask';
-COMMIT;
-INSERT INTO company.employees (emp_name, phone, position, dept, region) VALUES ('thomask', '91112222', 'EMP', 'ENG', 'SG');
-INSERT INTO company.salary (emp_name, salary, sal_label) VALUES ('thomask', 50000, gen_sal_label('thomask'));
-COMMIT;
