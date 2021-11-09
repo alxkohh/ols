cd4da8f9d6605ee10ddad76748a4766dd26660ca
Joyce <jycyeo@yahoo.com.sg>
2021-11-09 21:14:38 +0800

    remove read salary for HR

--- 8-users/dongy.sql
+++ 8-users/dongy.sql
@@ -17,10 +17,9 @@ BEGIN
     SA_USER_ADMIN.SET_USER_LABELS (
         policy_name       => 'sal_policy',
         user_name         => 'dongy',
-        max_read_label    => 'DIR:ENG,HR,FIN:APAC',
+        max_read_label    => 'DIR:ENG,FIN:APAC',
         max_write_label   => 'DIR:ENG,FIN:APAC',
-        min_write_label   => 'EMP',
-        def_label         => 'DIR:ENG,HR,FIN:APAC'
+        min_write_label   => 'EMP'   
     );
 END;
 /
